e54727111cae05ab431acce7445208d365d37af6
AmmoMax <steelratmax@gmail.com>
2020-07-09 13:37:42 +0300

    fix header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: amayor <amayor@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/05 21:52:59 by amayor            #+#    #+#             */
-/*   Updated: 2020/07/08 21:34:56 by amayor           ###   ########.fr       */
+/*   Updated: 2020/07/08 22:22:21 by amayor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strnew(size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(char *s);
+char	*check_rest(char *rest, char **line);
+void	save_rest(char **rest, char **res_strchr);
 char	*ft_strcpy(char *dst, const char *src);
+int		get_next_line(int fd, char **line);
 void	ft_strclr(char *str);
 
 #endif
